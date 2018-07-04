52c3b9011265c99f857b4883eb4c5b802790bf65
banjac275 <banjac275@gmail.com>
2018-07-04 16:57:49 +0200

    uradjeno dosta po pitanju interfejsa, dodate neke stvari, samo jos da se provali kako da animiram ono govno

---
+++ src/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".nav_top--menu_btn").addEventListener("click", () => {
+  console.log("kk");
+  document.querySelector(".nav_sidebar--wrapper").classList.toggle("animated");
+});
