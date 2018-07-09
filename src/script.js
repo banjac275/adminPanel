b28536377f963d576ccd3e96c38f0f4cf8e4844d
banjac275 <banjac275@gmail.com>
2018-07-09 16:26:36 +0200

    odradjeno da se u sajdbaru gase kartice i da sve menjaju korisnici

--- src/script.js
+++ src/script.js
@@ -37,15 +37,14 @@ let selList = [".sidebar", ".btn--user", ".btn--notification", ".btn--task", ".b
 listenerSetter(selList); 
 
 function listenerSetter(list) {
-  list.forEach(element => {
+  list.forEach((element, ind) => {
     let query = document.querySelectorAll(element);
 
     query.forEach((el) => {
       let check = 0;
       
       el.children[0].addEventListener("click", (e) => {
-        e.stopPropagation();
-        check = animateHelper(el.children[1].children, check);
+        check = downsize(query, el.children[0], ind);
       });
     });
   });
@@ -58,9 +57,11 @@ function animateHelper(elementList, check) {
       (elementList[i].parentNode.parentNode.children[0].children[1] === undefined) ? child = elementList[i].parentNode.parentNode.children[0].children[0] : child = elementList[i].parentNode.parentNode.children[0].children[1];
       elementList[i].classList.add("height-expand");
       elementList[i].parentNode.classList.remove("no-border");
-      child.classList.remove("fa-rotate-90");
       elementList[i].classList.remove("height-shrink");
-      setTimeout(() => {elementList[i].style.display = "block"}, 200);
+      setTimeout(() => {
+        elementList[i].style.display = "block";
+        child.classList.remove("fa-rotate-90");
+      }, 200);
     } 
     return check = true;
   } else {
@@ -69,10 +70,78 @@ function animateHelper(elementList, check) {
       (elementList[i].parentNode.parentNode.children[0].children[1] === undefined) ? child = elementList[i].parentNode.parentNode.children[0].children[0] : child = elementList[i].parentNode.parentNode.children[0].children[1];
       elementList[i].classList.add("height-shrink");
       elementList[i].parentNode.classList.add("no-border");
-      child.classList.add("fa-rotate-90");
       elementList[i].classList.remove("height-expand");
-      setTimeout(() => {elementList[i].style.display = "none"}, 200);
+      setTimeout(() => {
+        elementList[i].style.display = "none";
+        child.classList.add("fa-rotate-90");
+      }, 200);
     }
     return check = false;
+  }
+}
+
+function user(type) {
+  let userLoc = document.querySelector(".user--name");
+  let sidebarUsers = document.querySelector(".sidebar_users");
+  let sidebarEvents = document.querySelector(".sidebar_events");
+  let sidebarCompanies = document.querySelector(".sidebar_companies");
+  switch(type) {
+    case 'Banji':
+      userLoc.innerHTML = "Nikola Stevanović";
+      sidebarUsers.style.display = "block";
+      sidebarEvents.style.display = "block";
+      sidebarCompanies.style.display = "block";
+      break;
+    case 'moderator':
+      userLoc.innerHTML = "Moderator";
+      sidebarUsers.style.display = "none";
+      sidebarEvents.style.display = "block";
+      sidebarCompanies.style.display = "none";
+      break;
+    case 'super':
+      userLoc.innerHTML = "Super Admin (Aćim)";
+      sidebarUsers.style.display = "block";
+      sidebarEvents.style.display = "block";
+      sidebarCompanies.style.display = "block";
+      break;
+    default:
+      userLoc.innerHTML = "Random korisnik";
+      sidebarUsers.style.display = "block";
+      sidebarEvents.style.display = "none";
+      sidebarCompanies.style.display = "block";
+      break;
+  }
+}
+
+user("Banji");
+
+let dropElements = ['.dropdown_wrapper'];
+
+function clearDisplay(elems) {
+  (document.body || document.documentElement).addEventListener("click", (e) => {
+    elems.forEach((el) => {
+      let tmp = document.querySelectorAll(el);
+      tmp.forEach((t) => {
+        if(e.target !== t) t.style.display = "none";
+      });
+    });
+  }, false);
+}
+
+//clearDisplay(dropElements);
+
+function downsize(parent, element, index) {
+  if(index === 0) {
+    parent.forEach(el => {
+      if(el.children[0] !== element) {
+        if(el.children[1].children[0].classList.contains("height-expand")) animateHelper(el.children[1].children, true);
+      } else {
+        if(el.children[1].children[0].classList.contains("height-shrink")) return animateHelper(el.children[1].children, false);
+        else return animateHelper(el.children[1].children, true);
+      }
+    });
+  } else {
+    console.log(parent);
+    console.log(element.parentNode);
   }
 }
