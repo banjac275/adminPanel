7f04cb43eaae332fd324889791fa5de4ebf7b393
banjac275 <banjac275@gmail.com>
2018-07-10 15:16:10 +0200

    sve odradjeno

--- src/script.js
+++ src/script.js
@@ -24,18 +24,6 @@ document.querySelector(".nav_top--menu_btn").addEventListener("click", (e) => {
 
 });
 
-let infoMessage = document.querySelectorAll(".info--message");
-
-infoMessage.forEach((el) => {
-  let temp = el.innerHTML;
-  el.innerHTML = temp.slice(0, 20);
-  el.innerHTML += "...";
-});
-
-let selList = [".sidebar", ".btn--user", ".btn--notification", ".btn--task", ".btn--mail"];
-
-listenerSetter(selList); 
-
 function listenerSetter(list) {
   list.forEach((element, ind) => {
     let query = document.querySelectorAll(element);
@@ -85,63 +73,76 @@ function user(type) {
   let sidebarUsers = document.querySelector(".sidebar_users");
   let sidebarEvents = document.querySelector(".sidebar_events");
   let sidebarCompanies = document.querySelector(".sidebar_companies");
+  let sidebarWrapper = document.querySelector(".nav_sidebar--wrapper");
   switch(type) {
     case 'Banji':
       userLoc.innerHTML = "Nikola Stevanović";
       sidebarUsers.style.display = "block";
       sidebarEvents.style.display = "block";
       sidebarCompanies.style.display = "block";
+      sidebarWrapper.style.backgroundColor = "#1b1b1b";
       break;
     case 'moderator':
       userLoc.innerHTML = "Moderator";
       sidebarUsers.style.display = "none";
       sidebarEvents.style.display = "block";
       sidebarCompanies.style.display = "none";
+      sidebarWrapper.style.backgroundColor = "#ce9504";
       break;
     case 'super':
       userLoc.innerHTML = "Super Admin (Aćim)";
       sidebarUsers.style.display = "block";
       sidebarEvents.style.display = "block";
       sidebarCompanies.style.display = "block";
+      sidebarWrapper.style.backgroundColor = "#f36a5a";
       break;
     default:
       userLoc.innerHTML = "Random korisnik";
       sidebarUsers.style.display = "block";
       sidebarEvents.style.display = "none";
       sidebarCompanies.style.display = "block";
+      sidebarWrapper.style.backgroundColor = "#2b333e";
       break;
   }
 }
 
-user("Banji");
-
-let dropElements = ['.dropdown_wrapper'];
-
-function clearDisplay(elems) {
+function clearDisplay() {
+  let dropElements = document.querySelectorAll(".dropdown");
   (document.body || document.documentElement).addEventListener("click", (e) => {
-    elems.forEach((el) => {
-      let tmp = document.querySelectorAll(el);
-      tmp.forEach((t) => {
-        if(e.target !== t) t.style.display = "none";
-      });
+    dropElements.forEach((el) => {
+      if(!el.contains(event.target))
+        if(el.children[1].children[0].classList.contains("height-expand")) animateHelper(el.children[1].children, true);
     });
   }, false);
 }
 
-//clearDisplay(dropElements);
-
 function downsize(parent, element, index) {
-  if(index === 0) {
-    parent.forEach(el => {
-      if(el.children[0] !== element) {
-        if(el.children[1].children[0].classList.contains("height-expand")) animateHelper(el.children[1].children, true);
-      } else {
-        if(el.children[1].children[0].classList.contains("height-shrink")) return animateHelper(el.children[1].children, false);
-        else return animateHelper(el.children[1].children, true);
-      }
-    });
-  } else {
-    console.log(parent);
-    console.log(element.parentNode);
-  }
+  let navRight = document.querySelectorAll(".dropdown");
+  if(index !== 0) parent = navRight;
+  parent.forEach(el => {
+    if(el.children[0] !== element) {
+      if(el.children[1].children[0].classList.contains("height-expand")) animateHelper(el.children[1].children, true);
+    } else {
+      if(el.children[1].children[0].classList.contains("height-shrink")) return animateHelper(el.children[1].children, false);
+      else return animateHelper(el.children[1].children, true);
+    }
+  });
 }
+
+//pozivi fja
+
+user("");
+
+clearDisplay();
+
+let selList = [".sidebar", ".btn--user", ".btn--notification", ".btn--task", ".btn--mail"];
+
+listenerSetter(selList); 
+
+let infoMessage = document.querySelectorAll(".info--message");
+
+infoMessage.forEach((el) => {
+  let temp = el.innerHTML;
+  el.innerHTML = temp.slice(0, 20);
+  el.innerHTML += "...";
+});
